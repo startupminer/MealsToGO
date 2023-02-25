d5ba0454358a9a9c461466fb6950bc1c1bff2a1d
Virquan Harold <startupminer@gmail.com>
2023-02-25 02:11:59 -0500

    add custom fonts and ratings svg

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantScreen } from "./src/features/restuarants/screens/restaurant.screen";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/features/restuarants/components/restaurant-info-card.component.js
+++ src/features/restuarants/components/restaurant-info-card.component.js
@@ -2,16 +2,34 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Avatar, Button, Card, Text } from "react-native-paper";
 import styled from "styled-components";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
-const Title = styled.Text`
+const InfoDescription = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  padding: ${(props) => props.theme.space[2]};
+`;
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
   padding: ${(props) => props.theme.space[2]};
+`;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const ImageView = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding: ${(props) => props.theme.space[2]};
 `;
 
 // restaurant needs to be passed in empty then set the defaults
@@ -22,18 +40,28 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     photos = [
       "https://lh5.googleusercontent.com/p/AF1QipOeVHTzV20qwFfhN2Umxn0rIpyN55fOjOeVzh22=w228-h228-n-k-no",
     ],
+    description = "American-style gourmet burger joint. Family-owned since 1999 and famous for juicy burgers and cajun fries!",
     address = "555 Foodway",
-    openingHours = "10AM - 12PM",
+    openingHours = "HOURS: 10AM - 12PM",
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <Card>
       <ImageView>
         <Card.Cover key={name} source={{ uri: photos[0] }} />
       </ImageView>
-      <Title>{address}</Title>
       <Card.Title title={name} subtitle={openingHours} left={LeftContent} />
+      <Info>
+        <Address>{address}</Address>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+        <InfoDescription>{description}</InfoDescription>
+      </Info>
       {/* <Card.Content>
         <Text variant="titleLarge">Card title</Text>
         <Text variant="bodyMedium">Card content</Text>
