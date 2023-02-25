6bc20a0b5254f33f0f338d2c7bab0a454b02821a
Virquan Harold <startupminer@gmail.com>
2023-02-25 16:15:12 -0500

    export styles

--- src/features/restuarants/components/restaurant-info-card.styles.js
+++ src/features/restuarants/components/restaurant-info-card.styles.js
@@ -1,39 +1,39 @@
 import { styled } from "styled-components/native";
 
-const InfoDescription = styled.Text`
+export const InfoDescription = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
   padding: ${(props) => props.theme.space[2]};
 `;
-const Address = styled.Text`
+export const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.heading};
   font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
   padding: ${(props) => props.theme.space[2]};
 `;
-const Info = styled.View`
+export const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const ImageView = styled.View`
+export const ImageView = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const Rating = styled.View`
+export const Rating = styled.View`
   flex-direction: row;
   padding: ${(props) => props.theme.space[2]};
 `;
 
-const OpenIcon = styled.View`
+export const OpenIcon = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: flex-end;
 `;
 
-const Section = styled.View`
+export const Section = styled.View`
   flex-direction: row;
 `;
-const SectionRight = styled.View`
+export const SectionRight = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: flex-end;
